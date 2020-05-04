3f22f2dcc2973fe35bef2953fef4b3bb081b21e4
ronniegnr <gnr.ronnie@gmail.com>
2020-05-04 22:44:55 +0600

    Initial commit for spring-boot-markdown-editor-poc project

---
+++ spring-boot-markdown-editor-poc/src/main/java/bd/com/ronnie/springbootmarkdowneditorpoc/SpringBootMarkdownEditorPocApplication.java
@@ -0,0 +1,13 @@
+package bd.com.ronnie.springbootmarkdowneditorpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMarkdownEditorPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMarkdownEditorPocApplication.class, args);
+    }
+
+}
---
+++ spring-boot-markdown-editor-poc/src/test/java/bd/com/ronnie/springbootmarkdowneditorpoc/SpringBootMarkdownEditorPocApplicationTests.java
@@ -0,0 +1,13 @@
+package bd.com.ronnie.springbootmarkdowneditorpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMarkdownEditorPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
