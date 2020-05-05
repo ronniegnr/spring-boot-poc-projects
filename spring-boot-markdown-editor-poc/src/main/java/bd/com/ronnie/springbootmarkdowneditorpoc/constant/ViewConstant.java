1d1b0895960db36bfbb2e12d44fa33f9835e8f36
ronniegnr <gnr.ronnie@gmail.com>
2020-05-05 22:39:51 +0600

    markdown poc done with editormd

--- spring-boot-markdown-editor-poc/src/main/java/bd/com/ronnie/springbootmarkdowneditorpoc/constant/ViewConstant.java
+++ spring-boot-markdown-editor-poc/src/main/java/bd/com/ronnie/springbootmarkdowneditorpoc/constant/ViewConstant.java
@@ -5,7 +5,7 @@ public final class ViewConstant {
     private static final String POST_VIEW_PATH = "post/";
     public static final String POST_LIST_VIEW = POST_VIEW_PATH + "post-list";
     public static final String POST_DETAIL_VIEW = POST_VIEW_PATH + "post-detail";
-    public static final String POST_EDIT_VIEW = POST_VIEW_PATH + "post-edit";
+    public static final String POST_ENTRY_VIEW = POST_VIEW_PATH + "post-entry";
 
     private ViewConstant() {
     }
--- spring-boot-markdown-editor-poc/src/main/java/bd/com/ronnie/springbootmarkdowneditorpoc/web/controller/PostController.java
+++ spring-boot-markdown-editor-poc/src/main/java/bd/com/ronnie/springbootmarkdowneditorpoc/web/controller/PostController.java
@@ -3,8 +3,11 @@ package bd.com.ronnie.springbootmarkdowneditorpoc.web.controller;
 import bd.com.ronnie.springbootmarkdowneditorpoc.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static bd.com.ronnie.springbootmarkdowneditorpoc.constant.ViewConstant.POST_DETAIL_VIEW;
+import static bd.com.ronnie.springbootmarkdowneditorpoc.constant.ViewConstant.POST_ENTRY_VIEW;
 import static bd.com.ronnie.springbootmarkdowneditorpoc.constant.ViewConstant.POST_LIST_VIEW;
 
 @RequestMapping("posts")
@@ -20,6 +23,21 @@ public class PostController {
     @GetMapping("list")
     public String getList() {
         return POST_LIST_VIEW;
+    }
+
+    @GetMapping("entry")
+    public String getEntry() {
+        return POST_ENTRY_VIEW;
+    }
+
+    @PostMapping("entry")
+    public String postEntry() {
+        return POST_ENTRY_VIEW;
+    }
+
+    @GetMapping("detail")
+    public String getDetail() {
+        return POST_DETAIL_VIEW;
     }
 
 }
