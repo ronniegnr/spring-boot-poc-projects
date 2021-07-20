fa63c0bd4dd9edef6235e06c290e7c67d0230278
ronniegnr <gnr.ronnie@gmail.com>
2021-07-20 23:04:24 +0600

    spring-security project added

---
+++ spring-boot-spring-sequrity/src/main/java/bd/com/ronnie/springbootspringsequrity/SpringBootSpringSequrityApplication.java
@@ -0,0 +1,13 @@
+package bd.com.ronnie.springbootspringsequrity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringSequrityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSpringSequrityApplication.class, args);
+    }
+
+}
